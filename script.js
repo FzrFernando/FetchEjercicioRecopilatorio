b24dca98039f91e31f2129fd9e45eb9fb7e6f6a3
Fernando Zamora <fernandozamorarico@gmail.com>
2023-11-02 14:23:12 +0100

    Completado

--- script.js
+++ script.js
@@ -21,13 +21,104 @@ document.addEventListener("DOMContentLoaded", function () {
     })
     .catch(error => console.log(error))
 
+    
     // Añadir usuario
-    fetch(`${URL}/users`,{
-        method: 'POST',
-        body: JSON.stringify(),
-        headers: {
-            'Content-Type':'application/json'
+    userForm.addEventListener('submit', (event)=> {
+        event.preventDefault()
+        const user = {
+            name : document.getElementById("name").value,
+            address : document.getElementById("address").value,
+            email : document.getElementById("email").value
         }
+
+        if (user.name && user.address && validateEmail(user.email)){
+            
+            if (userForm.dataset.editing) {
+                let userId = event.target.dataset.id;
+
+                fetch(`${URL}/users/${userId}`,{
+                    method:'PUT',
+                    body: JSON.stringify(user),
+                    headers: {
+                        'Content-Type':'application/json'
+                    }
+                })
+                .then(response => {
+                    if (response.ok) {
+                        this.location.reload();
+                        return response.json();
+                    }
+                    return Promise.reject(response)
+                })
+                .catch(err => {
+                    console.log('Error en la petición HTTP:' + err.message);
+                })
+            } else {
+                fetch(`${URL}/users`,{
+                    method: 'POST',
+                    body: JSON.stringify(user),
+                    headers: {
+                        'Content-Type':'application/json'
+                    }
+                })
+                .then(response => {
+                    if (response.ok) {
+                        const listItem = document.createElement("li");
+                        listItem.innerHTML = `${user.name} : ${user.address} : ${user.email} : <button class="edit" value="${user.id}">Editar</button> <button class="delete" value="${user.id}">Delete</button>`;
+                        userList.appendChild(listItem);
+                        return response.json();
+                    }
+                    return Promise.reject(response)
+                })
+                .catch(err => {
+                    console.log('Error en la petición HTTP:' + err.message);
+                })
+            }
+        }
+        userForm.reset()
     })
 
+    function deleteUser(event){
+        if (event.target.classList.contains("delete")) {
+            const listItem = event.target.parentElement;
+            let userId = event.target.getAttribute("value");
+            fetch(`${URL}/users/${userId}`,{
+                method: 'DELETE'
+            })
+            .then(response => {
+                if (response.ok) {
+                    listItem.remove();
+                    return response.json();
+                }
+                return Promise.reject(response)
+            })
+            .catch(err => {
+                console.log('Error en la petición HTTP:' + err.message);
+            })
+        }
+    }
+
+    function editUser(event){
+        if (event.target.classList.contains("edit")){
+            const listItem = event.target.parentElement;
+            const userArray = listItem.textContent.split(" : ");
+            const name = userArray[0];
+            const address = userArray[1];
+            const email = userArray[2];
+            const id = event.target.getAttribute("value");
+            
+            userForm.setAttribute("data-id",id);
+            userForm.elements.name.value = name;
+            userForm.elements.address.value = address;
+            userForm.elements.email.value = email;
+            userForm.dataset.editing = email;
+            userForm.querySelector("button[type='submit']").textContent = "Editar Usuario";
+            userForm.dataset.editingIndex = [...userList.children].indexOf(listItem);
+        }
+    }
+
+    userList.addEventListener("click", function (event) {
+        deleteUser(event);
+        editUser(event);
+    });
 })
